66039f202ff04ba7345e93a6c5f3ffbe048f81a5
Mustafa1292 <35036281+Mustafa1292@users.noreply.github.com>
2021-03-03 15:04:45 -0600

    Updated menu
    Menu looks better.
    Deleted functions from switch option  6 (not needed)

--- main.cpp
+++ main.cpp
@@ -1,81 +1,69 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "client.h"
 #include "admin.h"
-//#include "source.cpp"
 
 using namespace std; 
 
-
-
 int customer_input;
 void customer_menu() {
-        
-    cout << "Please make a Selection:";
+        cout << "      _____________________________________________\n";
+        cout << "     |                                             |\n";
+        cout << "     |                                             |\n";
+        cout << "     |              C++ Bank project               |\n";
+        cout << "     |            Fast, safe and secure            |\n";
+        cout << "     |                                             |\n";
+        cout << "     |_____________________________________________|\n";
 
-    cout << "\n1 - Create an account\n"
-         << "2 - Deposit\n"
-         << "3 - Withdrawal\n"
-         << "4 - Transfer\n"
-         << "5 - Delete account\n"
-         << "6 - Exit\n";
+
+
+        cout << "Please make a Selection:\n";
+
+    cout << "\n1   -      Create an account\n"
+         << "2   -      Deposit\n"
+         << "3   -      Withdrawal\n"
+         << "4   -      Transfer\n"
+         << "5   -      Delete account\n"
+         << "6   -      Exit\n";
 }
 
 int main (){
     customer_menu();
     cin >> customer_input;
 
-    while (customer_input != 6) {
+    while (customer_input != 6) {               //Functions for all 6 cases will be added from the header files.
         
         switch(customer_input) {
             case 1:
-                system("clear");
-                //client new_customer;
-                // string name = "";
-                // int passcode = 0;
-                // int bal = 0;
-                // cout << "Please Enter your new username\n";
-                // cin >> name;
-                // cout << "Please Enter your new Password\n"; 
-                // cin >> passcode;
-                // cout << "Please enter your current balance\n";
-                // cin >> bal;
-
-                //new_customer.new_account(name, bal, passcode);
+                system("CLS");
                 cout << "case 1\n";
-                customer_menu();
-                cin >> customer_input;
-                break;
+                    customer_menu();
+                    cin >> customer_input;
+                    break;
             case 2:
-                system("clear");
-                cout << "Please Enter the Username\n";
+                system("CLS");
                 cout << "case 2\n";
                     customer_menu();
                     cin >> customer_input;
                     break;
             case 3:
-                system("clear");
+                system("CLS");
                 cout << "case 3\n";
                     customer_menu();
                     cin >> customer_input;
                     break;
             case 4:
-                system("clear");
+                system("CLS");
                 cout << "case 4\n";
                     customer_menu();
                     cin >> customer_input;
                     break;
             case 5:
-                system("clear");
+                system("CLS");
                 cout << "case 5\n";
                     customer_menu();
                     cin >> customer_input;
                     break;
-            case 6:
-                system("clear");
-                cout << "case 6\n";
-                    customer_menu();
-                    cin >> customer_input;
-                    break;
+            
             default:
                 cout << "Not a valid option please make a selection from the menu";
                 customer_menu();
